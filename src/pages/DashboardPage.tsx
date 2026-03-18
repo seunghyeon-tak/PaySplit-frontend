343c9b950437f78093171d5cf56b8c43352b79f0
mason <conficker77@gmail.com>
2026-03-18 22:57:19 +0900

    feat : React Router 세팅 및 기본 페이지 생성

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,5 @@
+const DashboardPage = () => {
+    return <h1>대시보드</h1>
+};
+
+export default DashboardPage;
---
+++ src/pages/SubscriptionPage.tsx
@@ -0,0 +1,5 @@
+const SubscriptionPage = () => {
+    return <h1>구독 목록</h1>;
+}
+
+export default SubscriptionPage;
